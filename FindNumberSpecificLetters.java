981757a5e2950615e411b847c0e2d4d03d7bed9d
Indu <pinschen@gmail.com>
2014-12-27 01:57:06 +0000

    Changed theJava code program Find number of specific letters in a string and includes a test to check it works

--- FindNumberSpecificLetters.java
+++ FindNumberSpecificLetters.java
@@ -1,22 +1,35 @@
-
 public class FindNumberSpecificLetters {
-
 	/**
 	 * @param args
 	 */
-	
-	public class String letterFinder(String userSource) {
-		
-		int index = o;
-		int letterCount;
-		
+	private static int letterFinder(String word, char targetLetter) {
+
+		int count = 0;
+
+		for (int index = 0; index < word.length(); index++) {
+			char currentLetter = word.charAt(index);
+			
+			if (currentLetter == targetLetter)
+				count++;
+		}
+		return count;
 	}
-		return letterCount;
-		
-	
-	
+
+	private static void testingPad(String wd, char tl, int expected) {
+		int answer = letterFinder(wd, tl);
+		if (answer == expected)
+			System.out.println("Hello, well done, the answer is " + expected
+					+ " as expected");
+		else
+			System.out
+					.println("Oh dear, it's wrong as you were expecting the answer: "
+							+ expected + " but got the answer: " + answer);
+	}
+
 	public static void main(String[] args) {
-
-
+		testingPad("", 'p', 0);
+		testingPad("bacon", 'c', 1);
+		testingPad("eggs", 'g', 2);
+		testingPad("coffee", 'e', 2);
 	}
 }
